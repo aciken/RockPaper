514c9b4a14874a0e6de82a12738985d168d88aa4
aciken <adrianmarton2006@gmail.com>
2023-10-29 12:02:25 +0100

    added some funtions and added idea for the next task

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 RockPaper start
 
 Added all mechanics to program, it works in console
+
+Addded score counter
+Added game mechanic(play 5 games in a row and declare the winner in general);
+Last thing that i need to add is for player to enter choice with prompt
--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 let computerChoice;
 let playerChoice = 'Scissors';
+let win = 0;
+let lose = 0;
+let draw = 0;
 
 const getRandomInt =(max) =>{
     return Math.floor(Math.random() * max);
   }
+
 
 const getComputerChoice = () =>{
     let number = getRandomInt(3) + 1; 
@@ -16,36 +20,66 @@ const getComputerChoice = () =>{
     }
     return computerChoice;
 }
+
+
 computerChoice = getComputerChoice();
 
-const play = (playerSelection, computerSelection) =>{
+const play = (playerSelection) =>{
+
+let computerSelection = getComputerChoice();
+
+
+console.log(`Your selection: ${playerSelection}`);
+console.log(`Computer selection: ${computerSelection}`);
+
     if(playerSelection === 'Rock'){
         if(computerSelection === 'Scissors'){
             console.log('You won, Rock beats Scissors')
+            win++;
         } else if(computerSelection === 'Paper'){
             console.log('You lose, Paper beats Rock');
+            lose++;
         } else {
             console.log('Draw');
+            draw++
         }
     } else if(playerSelection === 'Paper'){
         if(computerSelection === 'Rock'){
             console.log('You won, Paper beats Rock');
+            win++;
         } else if(computerSelection === 'Scissors'){
             console.log('You lose, Scissors beat Paper');
+            lose++;
         } else {
             console.log('Draw');
+            draw++;
         }  
     } else{
         if(computerSelection === 'Paper'){
             console.log('You won, Scissprs beat Paper');
+            win++;
         } else if(computerSelection === 'Rock'){
             console.log('You lose, Rock beats Scissors');
+            lose++;
         } else {
             console.log('Draw');
+            draw++;
         }
     }
 }
 
-console.log(playerChoice);
-console.log(computerChoice);
-play(playerChoice,computerChoice);
+
+const game = () =>{
+    for(let i = 0; i < 5; i++){
+       play(playerChoice);
+}
+if(win > lose){
+    console.log('YOU HAVE WON A GAME');
+} else if(lose > win){
+    console.log('YOU HAVE LOST A GAME');
+} else {
+    console.log('THE GAME IS DRAW');
+}
+}
+
+game();
